0385c21d261f151920b3f2b410ad57362f6cac1c
sn777 <3080063481@qq.com>
2017-06-25 19:59:37 +0800

    Update Test.java

--- Git/src/com/itheima/demo/Test.java
+++ Git/src/com/itheima/demo/Test.java
@@ -2,6 +2,6 @@ package com.itheima.demo;
 
 public class Test {
      public static void main(String[] args) {
-		System.out.println("java工程导出到了本地仓库");
+		System.out.println("java工程导出到了本地仓库2222");
 	}
 }
